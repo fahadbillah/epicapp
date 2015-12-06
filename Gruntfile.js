536d0e90c992ca0be568cc14ac852c8ff87c4423
fahadbillah <fahadbillah@yahoo.com>
2015-12-06 18:18:36 +0600

    so far all tool working without jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+'use strict';
 module.exports = function(grunt) {
 	
 	grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -5,15 +6,25 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-wiredep');
+	grunt.loadNpmTasks('grunt-file-creator');
+	grunt.loadNpmTasks('grunt-ng-annotate');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			allJs: {
+				src: ['app/src/**/*js'],
+				dest: 'dist/src/build.min.js'
+			}
+		},
 		uglify: {
 			options: {
 				banner: '/* <%= pkg.name %> build <%= grunt.template.today("yyyy-mm-dd") %> */'
 			},
 			build: {
-				src: 'app/src/*.js',
+				src: 'dist/src/build.min.js',
 				dest: 'dist/src/build.min.js'
 			}
 		},
@@ -49,17 +60,39 @@ module.exports = function(grunt) {
 		},
 		jshint: {
 			options: {
-				globals: {
-					'$': true,
-					'angular': true,
-					'EpicApp': true,
-				},
+				jshintrc: '.jshintrc',
 				reporter: require('jshint-stylish')
 			},
-			target: ['app/src/**']
+			target: ['GruntFile.js','app/src/']
+		},
+		'file-creator': {
+			options: {
+				openFlags: 'w'
+			},
+			basic: {
+				'dist/src/build.min.js': function(fs, fd, done) {
+					fs.writeSync(fd, '');
+					done();
+				}
+			}
+		},
+		ngAnnotate: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'dist/src/',
+					src: '*.js',
+					dest: 'dist/src/'
+				}]
+			}
+		},
+		wiredep: {
+			app: {
+				src: ['app/index.html']
+			}
 		},
 		clean: ["dist/"]
 	});
 
-	grunt.registerTask('default', ['clean','jshint','uglify','copy','htmlmin']);
+grunt.registerTask('default', ['clean','wiredep','copy','file-creator','concat','ngAnnotate','uglify','htmlmin']);
 };
